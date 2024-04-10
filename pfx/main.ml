bea0d70e68aa13e1159ee23381f6423e07033b46
khadija-b-a <71980070+khadija-b-a@users.noreply.github.com>
2024-04-11 00:04:16 +0200

    Update main.ml

--- pfx/main.ml
+++ pfx/main.ml
@@ -1,13 +1,22 @@
+open BasicPFX.lexer
 
+let rec examine_all lexbuf =
+    let result = token lexbuf in
+    print_token result;
+    print_string " ";
+    match result with
+    | EOF -> ()
+    | _   -> examine_all lexbuf
 
-let compile file =
-print_string ("File "^file^" is being treated!\n");
-try
-let input_file = open_in file in
-let lexbuf = Lexing.from_channel input_file in
-examine_all lexbuf;
-print_newline ();
-close_in (input_file)
-with Sys_error _ ->
-print_endline ("Can't find file '" ^ file ^ "'")
-let _ = Arg.parse [] compile ""
+  let compile file =
+  print_string ("File "^file^" is being treated!\n");
+  try
+    let input_file = open_in file in
+    let lexbuf = Lexing.from_channel input_file in
+    examine_all lexbuf;
+    print_newline ();
+    close_in (input_file)
+  with Sys_error _ ->
+    print_endline ("Can't find file '" ^ file ^ "'")
+
+  let _ = Arg.parse [] compile ""
